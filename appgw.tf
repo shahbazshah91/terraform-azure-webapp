c5412bddc2f842d46f11196f4ef8cc3e62110e16
shahbaz <shahbazahmedshah91@gmail.com>
2026-02-11 21:52:58 +0100

    modify locals

--- appgw.tf
+++ appgw.tf
@@ -23,48 +23,48 @@ resource "azurerm_application_gateway" "main" {
   }
 
   frontend_port {
-    name = local.frontend_port_name
+    name = local.app_gw.frontend_port_name
     port = 80
   }
 
   frontend_ip_configuration {
-    name                 = local.frontend_ip_configuration_name
+    name                 = local.app_gw.frontend_ip_configuration_name
     public_ip_address_id = azurerm_public_ip.appgw_ip.id
   }
 
   http_listener {
-    name                           = local.listener_name
-    frontend_ip_configuration_name = local.frontend_ip_configuration_name
-    frontend_port_name             = local.frontend_port_name
+    name                           = local.app_gw.listener_name
+    frontend_ip_configuration_name = local.app_gw.frontend_ip_configuration_name
+    frontend_port_name             = local.app_gw.frontend_port_name
     protocol                       = "Http"
   }
 
   backend_address_pool {
-    name = local.backend_address_pool_name
+    name = local.app_gw.backend_address_pool_name
     ip_addresses = [azurerm_network_interface.vm_nic.private_ip_address]
   }
 
   backend_http_settings {
-    name                  = local.http_setting_name
+    name                  = local.app_gw.http_setting_name
     cookie_based_affinity = "Disabled"
     #path                  = "/"
     port                  = 80
     protocol              = "Http"
     request_timeout       = 20
-    probe_name            = local.probe_name
+    probe_name            = local.app_gw.probe_name
   }
 
   request_routing_rule {
-    name                       = local.request_routing_rule_name
+    name                       = local.app_gw.request_routing_rule_name
     priority                   = 100
     rule_type                  = "Basic" #or PathBasedRouting
-    http_listener_name         = local.listener_name
-    backend_address_pool_name  = local.backend_address_pool_name
-    backend_http_settings_name = local.http_setting_name
+    http_listener_name         = local.app_gw.listener_name
+    backend_address_pool_name  = local.app_gw.backend_address_pool_name
+    backend_http_settings_name = local.app_gw.http_setting_name
   }
 
   probe {
-    name = local.probe_name
+    name = local.app_gw.probe_name
     #pick_host_name_from_backend_http_settings = true
     protocol = "Http"
     path = "/"
